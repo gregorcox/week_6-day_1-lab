b07c9f338766665581eaaeb949bc6df3bba8835c
Gregor Cox <gregorcox@gmail.com>
2018-06-25 15:15:53 +0100

    creates water bottle class and adds getVolume method

---
+++ src/main/java/WaterBottleTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() { waterBottle = new WaterBottle(100); }
+
+    @Test
+    public void hasVolume() {assertEquals(100, waterBottle.getVolume()); }
+
+}
